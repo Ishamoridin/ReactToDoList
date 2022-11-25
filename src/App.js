832395124a4412d4220e992ff66f9fb0c599cc0b
Ishamoridin <Ishamoridin@googlemail.com>
2022-11-25 13:03:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDoArray, refreshList] = useState(["Open fancy new to-do list", "Adjust to fancy font", "Start clearing finished tasks"])
+  
+  
+  const removeFromList = (index) => {
+    let tempArray=[...toDoArray];
+    tempArray.splice(index,1);
+    refreshList(tempArray)
+  }
+  const ToDoItem = (toDoArray) => {
+    const SetClass = (index) => {
+      switch (index%2){
+        case 0:return "light-todoitem";
+        case 1:return "dark-todoitem";
+        default:return "error-class";
+      }};
+          return (          
+            <>{toDoArray.map((listItem, index) => {
+              return (
+              <div key={index}  className="to-do-entry"><button onClick={() => {removeFromList(index)}} className={SetClass(index)} >✓</button><li>{listItem}</li></div>
+              )})}</>          
+          )
+
+}
+  const List = (props) => {
+    return (
+      <ul>
+        {ToDoItem(props.list)}
+      </ul>
+      
+    )
+  }
+  const AddItem = (listArray) => {
+    const [animation, setAnimation] = useState(false);
+    const [newItem, updateNewItem] = useState("");
+    const textChange = (e) => {
+      updateNewItem(e.target.value)
+    }
+    const addToList = (newItem) => {
+      let tempArray=[...toDoArray];
+      tempArray.push(newItem);
+      setAnimation(true);
+      refreshList(tempArray);
+    }
+    return (
+      <div 
+      setclass="input-area"
+      >
+      <form
+              onSubmit={() => {addToList(newItem)}}
+              className={animation ? "spin-once" : ""}
+              onAnimationEnd={() => setAnimation(false)}
+            
+            ><input     
+                    id="input-box"
+                    autoComplete='off'
+                    onChange={textChange} 
+                    placeholder="Append an errand"
+                  />
+      </form>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="main-area">
+    <h1>Tasks Remaining Unfinished</h1>
+    <AddItem />
+    <List list={toDoArray}/>
     </div>
   );
 }
+
+
+
+
 
 export default App;
